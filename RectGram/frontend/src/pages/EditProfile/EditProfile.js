0dfdea1a832aacea18ba6989bf86f141ff36f61c
Jeferson Soares <jpadilha53@gmail.com>
2023-04-24 18:09:39 -0300

    1.2

---
+++ RectGram/frontend/src/pages/EditProfile/EditProfile.js
@@ -0,0 +1,108 @@
+import "./EditProfile.css";
+
+import { uploads } from "../../utils/config";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+import { profile, resetMessage } from "../../slices/userSlice";
+
+// Components
+import Message from "../../components/Message";
+
+const EditProfile = () => {
+  const dispatch = useDispatch();
+
+  const { user, message, error, loading } = useSelector((state) => state.user);
+
+  // States
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [bio, setBio] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+
+  // Carregar os dados do usuário
+  useEffect(() => {
+    dispatch(profile());
+  }, [dispatch]);
+
+  // Preencha os dados do usuário do formulário
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      setBio(user.bio);
+    }
+  }, [user]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFile = (e) => {
+    // Imagem de Preview
+    const image = e.target.files[0];
+
+    setPreviewImage(image);
+
+    //Update do estado da imagem
+    setProfileImage(image);
+  };
+  return (
+    <div id="edit-profile">
+      <h2>Edite seus dados</h2>
+      <p className="subtitle">
+        Adicione uma imagem de pergil e conte mais sobre você...
+      </p>
+      {(user.profileImage || previewImage) && (
+        <img
+          src={
+            previewImage
+              ? URL.createObjectURL(previewImage)
+              : `${uploads}/users/${users.profileImage}`
+          }
+          alt={user.name}
+        ></img>
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""}
+        />
+        <input type="email" placeholder="E-mail" disabled value={email || ""} />
+        <label>
+          <span>Imagem de Perfil</span>
+          <input type="file" onChange={handleFile} />
+        </label>
+        <label>
+          <span>Bio</span>
+          <input
+            type="text"
+            placeholder="Descrição do perfil"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio || ""}
+          />
+        </label>
+        <label>
+          <span>Quer alterar sua senha?</span>
+          <input
+            type="password"
+            placeholder="Digite sua nova senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password || ""}
+          />
+        </label>
+        <input type="submit" value="Atualizar" />
+      </form>
+    </div>
+  );
+};
+
+export default EditProfile;
--- RectGram/frontend/src/store.js
+++ RectGram/frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import auhtReducer from "./slices/authSlice";
+import userReducer from "./slices/userSlice";
 
 export const store = configureStore({
   reducer: {
     auth: auhtReducer,
+    user: userReducer,
   },
 });
